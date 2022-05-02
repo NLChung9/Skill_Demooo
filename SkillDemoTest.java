4303b616b2048f64bbf69d7c691c416d4a430486
NLChung9 <88344454+NLChung9@users.noreply.github.com>
2022-05-01 23:43:00 -0700

    Fixed failed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,9 +1,12 @@
+//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" SkillDemoTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore SkillDemoTest
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTest {
     @Test
     public void multiply(){
-        assertEquals(999, SkillDemo.multiply(2,3));
+        assertEquals(6, SkillDemo.multiply(2,3));
     }
 }
